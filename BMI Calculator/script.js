6cbb65b397361fb573f4b6e222699038cdab0a82
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-10-02 18:24:06 +0500

    BMI calculator new project

---
+++ BMI Calculator/script.js
@@ -0,0 +1,11 @@
+
+function changeValue(){
+      const value = document.querySelector('.weight').value;
+      const bmi = document.querySelector('.bmi-container');
+      bmi.innerHTML = value;
+    }
+
+
+function resetValue(){
+    
+}
