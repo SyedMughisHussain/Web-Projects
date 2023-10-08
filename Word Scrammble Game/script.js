41ac6e80dab5d4e5072fdebd31f1b8ff256d9c81
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-10-08 22:22:22 +0500

    Changes made

--- Word Scrammble Game/script.js
+++ Word Scrammble Game/script.js
@@ -1,14 +1,42 @@
+document.querySelector(".totalScorelevel").innerHTML = 5;
+document.querySelector(".scrambleListWord").innerHTML = "aplep";
 
+let right = 0;
+let wrong = 0;
 
-function getSeconds(){
-    let date = new Date();
-    let val =  date.getSeconds();
-    console.log(date.toLocaleTimeString());
+function checkWord() {
+  let inputWord = document.querySelector(".input");
+  if (inputWord.value === "") {
+    alert("Enter a valid word.");
+  } else if (inputWord.value === "apple") {
+    document.querySelector(".yourRight").innerHTML = ++right;
+    inputWord.value = "";
+    document.querySelector(".scrambleListWord").innerHTML = "elephant";
+  } else if (inputWord.value === "elephant") {
+    document.querySelector(".yourRight").innerHTML = ++right;
+    inputWord.value = "";
+    document.querySelector(".scrambleListWord").innerHTML = "ogd";
+  } else if (inputWord.value === "dog") {
+    document.querySelector(".yourRight").innerHTML = ++right;
+    inputWord.value = "";
+    document.querySelector(".scrambleListWord").innerHTML = "tac";
+  } else if (inputWord.value === "cat") {
+    document.querySelector(".yourRight").innerHTML = ++right;
+    inputWord.value = "";
+    document.querySelector(".scrambleListWord").innerHTML = "wemo";
+  } else if (inputWord.value === "meow") {
+    document.querySelector(".yourRight").innerHTML = ++right;
+    inputWord.value = "";
+    document.querySelector(".scrambleListWord").innerHTML = "End";
+  } else if (
+    inputWord.value !== "apple" ||
+    inputWord.value !== "elephant" ||
+    inputWord.value !== "dog" ||
+    inputWord.value !== "cat" ||
+    inputWord.value !== "meow"
+  ) {
+    document.querySelector(".yourWrong").innerHTML = ++wrong;
+  } else {
+    alert("Wrong");
+  }
 }
-
-setInterval(getSeconds, 1000);
-
-
-
- 
-
