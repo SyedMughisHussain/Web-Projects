027ac26c3a6e2e5a323aad0969d9f93e58d9b078
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-10-13 12:33:08 +0500

    Rock Paper Scissors game made

--- Rock Paper Scissor Game/script.js
+++ Rock Paper Scissor Game/script.js
@@ -1,24 +1,50 @@
+let userChoice = document.querySelector(".user");
+let computer = document.querySelector(".computer");
+let string = document.querySelector(".string");
 
-let afterButtons = document.querySelector('.cont');
-let result = document.querySelector('.result');
+function random() {
+  let images = [
+    "./images/rock.png",
+    "./images/paper.png",
+    "./images/scissors.png",
+  ];
+  let index = Math.floor(Math.random() * images.length);
+  return [images[index], index];
+}
 
-let yourScore = 0;
-let computerScore = 0;
-
-function rock(){
-    let arr  = ['ROCK','PAPER','SCISSOR'];
-    let index = Math.floor(Math.random() * arr.length);
-    let val = arr[index];
-    let YourChoose = document.createElement('div');
-    YourChoose.innerHTML = `<p>You choose <b>ROCK</b></p>`;
-    afterButtons.appendChild(YourChoose);
-    let compChoose = document.createElement('div');
-    compChoose.innerHTML = `<p>Com choose <b>${val}</b></p>`;
-    afterButtons.appendChild(compChoose);
-    if(val === 'ROCK'){
-        let ele = document.createElement('p');
-        ele.innerHTML = `Draw`;
-        result.appendChild(ele);
-    }   
-
+function initGame(choice) {
+  if (choice === "rock") {
+    userChoice.src = "./images/rock.png";
+    let val = random();
+    computer.src = val[0];
+    if (val[1] === 0) {
+      string.innerHTML = "Draw!!";
+    } else if (val[1] === 1) {
+      string.innerHTML = "Cpu Won!!";
+    } else if (val[1] === 2) {
+      string.innerHTML = "User Won!!";
+    }
+  } else if (choice === "paper") {
+    userChoice.src = "./images/paper.png";
+    let val = random();
+    computer.src = val[0];
+    if (val[1] === 0) {
+      string.innerHTML = "User Won!!";
+    } else if (val[1] === 1) {
+      string.innerHTML = "Draw!!";
+    } else if (val[1] === 2) {
+      string.innerHTML = "Cpu Won!!";
+    }
+  } else if (choice === "scissor") {
+    userChoice.src = "./images/scissors.png";
+    let val = random();
+    computer.src = val[0];
+    if (val[1] === 0) {
+      string.innerHTML = "Cpu Won!!";
+    } else if (val[1] === 1) {
+      string.innerHTML = "User Won!!";
+    } else if (val[1] === 2) {
+      string.innerHTML = "Draw!!";
+    }
+  }
 }
